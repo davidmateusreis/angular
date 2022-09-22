fcb5d3b491171f81c93d290fb709a6b0a9c3e0ef
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-09-22 10:15:25 -0300

    Ciclo de vida do Componente

---
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,52 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  @Input() valorInicial: number = 10;
+
+  constructor() {
+    this.log('constructor');
+  }
+
+  ngOnChanges() { //se tiver input properties
+    this.log('ngOnChanges');
+  }
+
+  ngOnInit(): void { //chamada pro servidor para obter os dados mostrados no template do componente (se não tiver input properties)
+    this.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    this.log('AfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    this.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string) {
+    console.log(hook);
+  }
+
+}
