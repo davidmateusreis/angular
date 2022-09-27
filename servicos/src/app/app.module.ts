93f046af0a7063f3c403b24a3a5a1d0ae1038666
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-09-27 10:24:23 -0300

    Injetando um serviço em outro serviço

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CriarCursoModule,
     CursosModule
   ],
+  providers: [LogService],
   //providers: [CursosService], //serviços são declarados aqui
   bootstrap: [AppComponent]
 })
