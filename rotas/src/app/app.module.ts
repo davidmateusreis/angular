bf5819a89fe4bf971f4db97bf6865e550b10c769
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-09-29 14:11:36 -0300

    Criando um módulo de funcionalidade

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -6,26 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    CursosModule
   ],
-  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
