52056ba92aa73de4cd9b5b85061e66284655e8cb
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-09-29 10:17:18 -0300

    Rotas Imperativas: Redirecionamento via código

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
   { path: 'curso/:id', component: CursoDetalheComponent },
   { path: 'cursos', component: CursosComponent },
   { path: 'login', component: LoginComponent },
