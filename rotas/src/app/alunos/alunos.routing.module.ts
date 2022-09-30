17542009428c2dd2b95966e57b9deff43669c63a
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-09-30 08:58:58 -0300

    Rotas Filhas

---
+++ rotas/src/app/alunos/alunos.routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AlunoDetalheComponent } from "./aluno-detalhe/aluno-detalhe.component";
+import { AlunoFormComponent } from "./aluno-form/aluno-form.component";
+import { AlunosComponent } from "./alunos.component";
+
+const alunosRoutes: Routes = [
+    {
+        path: 'alunos', component: AlunosComponent, children: [ //rotas filhas
+            { path: 'novo', component: AlunoFormComponent },
+            { path: ':id', component: AlunoDetalheComponent },
+            { path: ':id/editar', component: AlunoFormComponent }
+        ]
+    },
+
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(alunosRoutes)],
+    exports: [RouterModule]
+})
+export class AlunosRoutingModule { }
