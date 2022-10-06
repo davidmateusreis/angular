d92de23777788184761e7ff127856719d6cf6942
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-10-06 08:13:22 -0300

    Definindo rota padrão e wildcard (rota não encontrada)

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CursosGuard } from './guards/cursos.guard';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 const routes: Routes = [
   {
@@ -21,7 +22,9 @@ const routes: Routes = [
     ), canActivate: [AuthGuard], canActivateChild: [AlunosGuard], canLoad: [AuthGuard]
   },
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] }
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PaginaNaoEncontradaComponent, } //canActivate: [AuthGuard] }
 ];
 
 @NgModule({
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { AuthGuard } from './guards/auth-guard';
 import { CursosGuard } from './guards/cursos.guard';
 import { AlunosGuard } from './guards/alunos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 //import { CursosModule } from './cursos/cursos.module';
 //import { AlunosModule } from './alunos/alunos.module';
 
@@ -18,7 +19,8 @@ import { AlunosGuard } from './guards/alunos.guard';
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
