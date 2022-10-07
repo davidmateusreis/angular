0c79111d6d8188bcc1d5dedf69f1d972033920b8
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-10-07 09:52:02 -0300

    Forms (template driven) Pesquisando endereço automaticamente com CEP

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -18,7 +20,7 @@ export class TemplateFormComponent implements OnInit {
     //console.log(this.usuario);
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -30,6 +32,22 @@ export class TemplateFormComponent implements OnInit {
   aplicaCssErro(campo: { valid: any; touched: any; }) {
     return {
       'alert alert-danger': this.verificaValidTouched(campo)
+    }
+  }
+
+  consultaCep(cep: any) {
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+      //Expressão regular para validar o CEP.
+      let validacep = /^[0-9]{8}$/;
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+          .pipe(map((dados: any) => dados))
+          .subscribe(dados => console.log(dados));
+      }
     }
   }
 
