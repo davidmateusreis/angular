f4b65165895c23f6fa58128956f0d7d491935f15
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-10-07 10:18:10 -0300

    Forms (template driven) Populando campos com setValue e patchValue (CEP)

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -35,7 +35,7 @@ export class TemplateFormComponent implements OnInit {
     }
   }
 
-  consultaCep(cep: any) {
+  consultaCep(cep: any, form: any) {
     //Nova variável "cep" somente com dígitos.
     cep = cep.replace(/\D/g, '');
     //Verifica se campo cep possui valor informado.
@@ -44,11 +44,53 @@ export class TemplateFormComponent implements OnInit {
       let validacep = /^[0-9]{8}$/;
       //Valida o formato do CEP.
       if (validacep.test(cep)) {
+
+        this.resetaDadosForm(form); //Reseta os campos do formulário
+
         this.http.get(`//viacep.com.br/ws/${cep}/json/`)
           .pipe(map((dados: any) => dados))
-          .subscribe(dados => console.log(dados));
+          .subscribe(dados => this.populaDadosForm(dados, form));
       }
     }
+  }
+
+  populaDadosForm(dados: any, formulario: any) {
+    /*formulario.setValue({
+      nome: formulario.value.nome,
+  email: formulario.value.email,
+  endereco: {
+    cep: dados.cep,
+    numero: '',
+    complemento: dados.complemento,
+    rua: dados.logradouro,
+    bairro: dados.bairro,
+    cidade: dados.localidade,
+    estado: dados.uf
+  }
+    });*/
+
+    formulario.form.patchValue({
+      endereco: {
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+
+  }
+
+  resetaDadosForm(formulario: any) {
+    formulario.form.patchValue({
+      endereco: {
+        complemento: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
