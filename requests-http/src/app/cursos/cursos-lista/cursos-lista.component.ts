5d82d34a18e5ed7591ccccb36d424bb3faa2e73c
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-10-14 14:00:17 -0300

    Http GET: listar registros

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Curso } from 'src/app/models/curso';
+import { CursosService } from '../cursos.service';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+  cursos!: Curso[];
+
+  constructor(private cursosService: CursosService) { }
 
   ngOnInit(): void {
+    this.cursosService.list()
+      .subscribe((dados: any) => this.cursos = dados);
   }
 
 }
