0b6305ff79d868c5ae3b2a0af73f52f81f006c15
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-10-20 09:13:17 -0300

    Http Upload de Arquivo: Request com FormData

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  files!: Set<File>;
+
+  constructor(private uploadFileService: UploadFileService) { }
 
   ngOnInit(): void {
   }
@@ -20,10 +23,19 @@ export class UploadFileComponent implements OnInit {
     //document.getElementById('customFileLabel')!.innerHTML = selectedFiles[0].name;
 
     const fileNames = [];
+    this.files = new Set();
     for (let i = 0; i < selectedFiles.length; i++) {
       fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
     }
     document.getElementById('customFileLabel')!.innerHTML = fileNames.join(', ');
+  }
+
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.uploadFileService.upload(this.files, 'http://localhost:8000/upload')
+        .subscribe(response => console.log('Upload concluído!'));
+    }
   }
 
 }
